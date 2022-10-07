4ef2c49f04df9b8a2f35586b9fb2ac496eb0a3d2
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-07 23:45:44 +0400

    Внесла поправки в задачу №2. Решена задача №2 с блок-схемой.

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -4,9 +4,14 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
-int a = 2, b = 10;
+int a = 5, b = 7;
 int max = a;
-if (max < b) max = b;
+int min = a;
+
+if ( a > b ) min = b;
+else max = b;
 
 Console.Write ("max = ");
 Console.WriteLine(max);
+Console.Write ("min = ");
+Console.WriteLine(min);
