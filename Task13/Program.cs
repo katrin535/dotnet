b16469fa88f5cdcd4e70e4f90a1692c3f21bdd46
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-09 00:27:31 +0400

    решена задача 10

---
+++ Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+Console.Write("Введите число: ");
+string str = Console.ReadLine();
+int a = Math.Abs(Convert.ToInt32(str));
+string c = Convert.ToString(a);
+if (c.Length > 2)
+{
+    Console.WriteLine("третья цифра числа: " + c[2]);
+}
+else 
+{
+    Console.WriteLine("третья цифра числа: отсутствует");
+}
+
---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите день недели цифрой от 1 до 7, где 1 - пон, 7 - воскр: ");
+string str = Console.ReadLine();
+if (str == "6" || str =="7")
+{
+    Console.WriteLine ("Да");
+}
+else if (str == "1" || str =="2" || str == "3" || str =="4" || str =="5")
+{
+     Console.WriteLine ("Нет");
+}
+else Console.WriteLine ("Что-то не то");
