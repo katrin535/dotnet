78f4bc146f08446a77f3c17a15c6d12f230509ed
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-09 00:20:08 +0400

    Решена задача №10 с блок-схемой

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+string str = Console.ReadLine();
+int a = Math.Abs(Convert.ToInt32(str));
+string c = Convert.ToString(a);
+
+Console.WriteLine("Вторая цифра числа: " + c[1]);
+     
+
+
+
+
+
+
