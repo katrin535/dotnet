cba1e7afe4df41867396dc8fb4523523e0ab0d4f
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-08 14:02:29 +0400

    Внесены последние корректировки по задаче 4

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -6,9 +6,18 @@
 
 22, 3, 9 -> 22*/
 
-int a = 44;
-int b = 5;
-int c = 78;
+Console.Write("Введите число a: ");
+string N = Console.ReadLine();
+int a = Convert.ToInt32(N);
+
+Console.Write("Введите число b: ");
+string K = Console.ReadLine();
+int b = Convert.ToInt32(K);
+
+Console.Write("Введите число c: ");
+string L = Console.ReadLine();
+int c = Convert.ToInt32(L);
+
 int max = a;
 
 if (b > max) max = b;
