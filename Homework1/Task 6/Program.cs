c1e76fde74972895c21f260f8205318ec1c6ea04
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-08 12:00:02 +0400

    Решение 6 задачи с блок-схемой

---
+++ Homework1/Task 6/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.Write("Введите число: ");
+string b = Console.ReadLine();
+int a = Convert.ToInt32(b);
+
+if (a % 2 == 0) 
+{
+    Console.Write(a);
+    Console.WriteLine(" - четное");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine(" - нечетное");
+}
